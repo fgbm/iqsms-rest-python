f69d8698bfab36734a83ffc9ba43ecfca62008a5
Vladislav <neimp@yandex.ru>
2021-05-30 00:04:10 +0300

    Исправлена ошибка при отправке на PYPI dirty-сборок (https://github.com/dolfinus/setuptools-git-versioning#dirty-template)

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 
 setup(
-    version_config=True,
+    version_config={
+        "dirty_template": "{tag}.dev{ccount}",
+    },
     setup_requires=['setuptools-git-versioning']
 )
